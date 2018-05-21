8421d713edf85358332c7ef26391f69020806f64
WingChhun <chhunjamesdc2@yahoo.com>
2018-05-21 13:25:03 -0700

    Finish to sectino 13, added eslint

--- src/App.js
+++ src/App.js
@@ -3,34 +3,26 @@ import logo from './logo.svg';
 import Movie from "./components/Movie";
 import './App.css';
 
-class App extends Component {
-    //DEFAULT STATE
-    state = {
+const movies = [
 
-        movies: []
-    }
-
-    //async await with the API
-    async componentDidMount()
     {
-        const apiKey = "adf9e0e13c83fa0507f265904cb93956";
-        const request = "https://api.themoviedb.org/3/discover/movie?api_key=adf9e0e13c83fa0507f265904cb9" +
-                "3956&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=fa" +
-                "lse&page=1";
-        try {
-            //make request, await data and stoer into res
-            const res = await fetch(request);
-            const movies = await res.json(); //store movies from res.json ,await for res to have something
-
-            this.setState({movies: movies.results});
-            console.log("This.state.movies", this.state.movies);
-        } catch (e) {
-            console.log(e);
-        }
+        id: 1,
+        title: 'star wars',
+        desc: "Best movie ever"
+    }, {
+        id: 2,
+        title: '13 reasons why',
+        desc: "Emo af movie"
+    }, {
+        id: 3,
+        title: 'Whiplash',
+        desc: "Never watched this movie"
     }
+];
+
+class App extends Component {
 
     render() {
-        const {movies} = this.state;
         return (
             <div className="App">
 
@@ -38,7 +30,7 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
 
                 </header>
-                {movies.map((movie, index) => <Movie key={movie.id} movie={movie}/>)}
+                {movies.map((movie, index) => <Movie key={index} movie={movie}/>)}
             </div>
         );
     }

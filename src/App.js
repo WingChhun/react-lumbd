bd51159c5d5adb6e81022b2f5dd26165a49873db
WingChhun <capt.wing.chhun@gmail.com>
2018-05-22 17:36:33 -0700

    GH-pages build, and added basename attribute with base url for hashrouter

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import MovieList from "./components/Movie/MoviesList";
 class App extends Component {
     render() {
         return (
-            <HashRouter>
+            <HashRouter basename={process.env.PUBLIC_URL}>
                 <div className="App">
                     <header className="App-header">
                         <img src={logo} className="App-logo" alt="logo"/>
